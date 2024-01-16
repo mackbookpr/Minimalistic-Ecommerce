18aa9288031d641d13cd5a029359f3667a4b50e1
MadhavMaheshwari1 <maheshwarimadhav166@gmail.com>
2024-01-16 13:44:15 +0530

    Men Section Added

--- src/Pages/MenSection.jsx
+++ src/Pages/MenSection.jsx
@@ -4,6 +4,7 @@ import { FaAngleDown } from "react-icons/fa6";
 
 function MenSection() {
   return (
+    <>
     <div className="container m-auto flex justify-between">
       <h2 className='font-bold text-[1.2em]'>Men's ethnic wear</h2>
       <div className='flex gap-5 items-center'>
@@ -11,6 +12,7 @@ function MenSection() {
         <button className='font-bold text-[1.2em]'><span>Sort by</span><FaAngleDown style={{fontSize:'1em',display:'inline'}}/></button >
       </div>
     </div>
+    </>
   )
 }
 
